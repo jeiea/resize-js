14d67675708c0afb9a8359103317247ccf0c50e7
jeiea <solum5013@gmail.com>
2019-03-01 05:14:06 +0900

    Add lint

--- src/resize.ts
+++ src/resize.ts
@@ -1,42 +1,47 @@
-﻿import { promises as fs } from 'fs';
-import * as path from 'path';
-import * as popen from 'child_process';
-import * as readline from 'readline';
+﻿import * as popen from "child_process";
+import { promises as fs } from "fs";
+import * as path from "path";
+import * as readline from "readline";
 
-async function wait_child(child) {
+async function wait_child(child: popen.ChildProcess) {
   return new Promise((resolve, reject) => {
-    child.on('exit', (code, signal) => {
-      if (code === null) reject(signal);
-      else resolve(code);
+    child.on("exit", (code, signal) => {
+      if (code === null) {
+        reject(signal);
+      }
+      else {
+        resolve(code);
+      }
     });
   });
 }
 
-// Throws error message
-async function seven_zip(...args) {
-  let msg = []
-  let exe = path.join(__dirname, 'exe/7za.exe');
-  let p7z = popen.spawn(exe, args);
-  p7z.stdout.on('data', d => { msg.push(d); });
-  p7z.stderr.on('data', d => { msg.push(d); });
-  let ret = await wait_child(p7z);
-  if (ret > 1) throw `7za error occured: ${ret}\n${Buffer.concat(msg)}`;
+async function seven_zip(...args: string[]) {
+  const msg = [];
+  const exe = path.join(__dirname, "exe/7za.exe");
+  const p7z = popen.spawn(exe, args);
+  p7z.stdout.on("data", d => { msg.push(d); });
+  p7z.stderr.on("data", d => { msg.push(d); });
+  const ret = await wait_child(p7z);
+  if (ret > 1) {
+    throw new Error(`7za error occured: ${ret}\n${Buffer.concat(msg)}`);
+  }
   return ret;
 }
 
-async function get_temp_and_files(arg) {
-  let entry = await fs.stat(arg);
-  if (entry.isFile() && arg.endsWith('.zip')) {
-    let out_dir = path.join(path.dirname(arg), path.basename(arg, '.zip'));
-    out_dir = path.resolve(out_dir)
-    await seven_zip('x', arg, '-y', '-o' + out_dir);
-    let fileNames = await fs.readdir(out_dir);
-    let paths = fileNames.map(x => path.join(out_dir, x));
-    return [out_dir, paths];
+async function get_temp_and_files(arg: string) {
+  const entry = await fs.stat(arg);
+  if (entry.isFile() && arg.endsWith(".zip")) {
+    let outDir = path.join(path.dirname(arg), path.basename(arg, ".zip"));
+    outDir = path.resolve(outDir);
+    await seven_zip("x", arg, "-y", "-o" + outDir);
+    const fileNames = await fs.readdir(outDir);
+    const paths = fileNames.map(x => path.join(outDir, x));
+    return [outDir, paths];
   }
   else if (entry.isDirectory()) {
-    let fileNames = await fs.readdir(arg);
-    let paths = fileNames.map(x => path.join(arg, x));
+    const fileNames = await fs.readdir(arg);
+    const paths = fileNames.map(x => path.join(arg, x));
     return [null, paths];
   }
   else {
@@ -44,67 +49,69 @@ async function get_temp_and_files(arg) {
   }
 }
 
-async function spawn_conv(file, optimize) {
-  let convert_opt = [file, '-normalize', '-resize', '1920x1080>']
-    .concat(optimize ? ['png:-'] : ['-define', 'webp:lossless=true', 'webp:-']);
-  let guetzli_opt = ['--quality', '95', '-', '-'];
+async function spawn_conv(file: string, optimize: boolean) {
+  const convertOpt = [file, "-normalize", "-resize", "1920x1080>"]
+    .concat(optimize ? ["png:-"] : ["-define", "webp:lossless=true", "webp:-"]);
+  const guetzliOpt = ["--quality", "95", "-", "-"];
 
-  let exe = path.join(__dirname, 'exe/convert.exe');
-  let convert = popen.spawn(exe, convert_opt);
-  convert.stderr.pipe(process.stdout);
-  let bufs = [];
+  let exe = path.join(__dirname, "exe/convert.exe");
+  const convProc = popen.spawn(exe, convertOpt);
+  convProc.stderr.pipe(process.stdout);
+  const bufs = [];
   if (optimize) {
-    exe = path.join(__dirname, 'exe/guetzli.exe');
-    let guetzli = popen.spawn(exe, guetzli_opt);
+    exe = path.join(__dirname, "exe/guetzli.exe");
+    const guetzli = popen.spawn(exe, guetzliOpt);
     guetzli.stderr.pipe(process.stdout);
-    guetzli.stdout.on('data', d => { bufs.push(d); });
-    convert.stdout.pipe(guetzli.stdin);
-    if (await wait_child(guetzli) != 0)
+    guetzli.stdout.on("data", d => { bufs.push(d); });
+    convProc.stdout.pipe(guetzli.stdin);
+    if (await wait_child(guetzli) !== 0) {
       await Promise.reject(`guetzli: ${file}`);
+    }
   }
   else {
-    convert.stdout.on('data', d => { bufs.push(d); });
-    if (await wait_child(convert) != 0)
+    convProc.stdout.on("data", d => { bufs.push(d); });
+    if (await wait_child(convProc) !== 0) {
       await Promise.reject(`convert: ${file}`);
+    }
   }
   return Buffer.concat(bufs);
 }
 
-async function determine_extension(file) {
-  let fd = await fs.open(file, 'r');
-  let buf = Buffer.alloc(12);
+async function determine_extension(file: string) {
+  const fd = await fs.open(file, "r");
+  const buf = Buffer.alloc(12);
   await fs.read(fd, buf, 0, buf.length, 0);
   await fd.close();
-  let sig = buf.toString();
-  if (sig.startsWith('\xFF\xD8\xFF')) {
-    return '.jpg';
+  const sig = buf.toString();
+  if (sig.startsWith("\xFF\xD8\xFF")) {
+    return ".jpg";
   }
-  if (sig.startsWith('PNG')) {
-    return '.png';
+  if (sig.startsWith("PNG")) {
+    return ".png";
   }
-  if (sig.startsWith('GIF')) {
-    return '.gif';
+  if (sig.startsWith("GIF")) {
+    return ".gif";
   }
-  if (sig.startsWith('RIFF') && sig.substr(8, 4) === 'WEBP') {
-    return '.webp';
+  if (sig.startsWith("RIFF") && sig.substr(8, 4) === "WEBP") {
+    return ".webp";
   }
   return null;
 }
 
-async function revise_pic(file) {
-  if (file.endsWith('.webp')) return;
+async function revise_pic(file: string) {
+  if (file.endsWith(".webp")) { return; }
 
-  let data = await spawn_conv(file, false);
-  let stat = await fs.stat(file);
+  const data = await spawn_conv(file, false);
+  const stat = await fs.stat(file);
 
-  let base = file.slice(0, file.lastIndexOf('.'));
+  const base = file.slice(0, file.lastIndexOf("."));
   if (stat.size < data.length) {
-    let ext = await determine_extension(file);
-    if (ext === null || file.endsWith(ext)) return;
+    const ext = await determine_extension(file);
+    if (ext === null || file.endsWith(ext)) { return; }
     return fs.rename(file, base + ext);
   }
 
-  let target = base + '.webp';
+  const target = base + ".webp";
   await fs.writeFile(target, data);
 
   if (file !== target) {
@@ -112,22 +119,26 @@ async function revise_pic(file) {
   }
 }
 
-async function rmrf(path) {
-  let proms = [];
-  for (let file of await fs.readdir(path)) {
-    let cur = path + '/' + file;
-    let is_dir = (await fs.lstat(cur)).isDirectory();
-    proms.push(is_dir ? rmrf(cur) : fs.unlink(cur));
+// tslint:disable-next-line: no-shadowed-variable
+async function rmrf(path: string) {
+  const proms = [];
+  for (const file of await fs.readdir(path)) {
+    const cur = path + "/" + file;
+    const isDir = (await fs.lstat(cur)).isDirectory();
+    proms.push(isDir ? rmrf(cur) : fs.unlink(cur));
   }
   await Promise.all(proms);
   await fs.rmdir(path);
-};
+}
 
-async function convert(args) {
-  let proms = [], folder_proms = [];
-  for (let arg of args) {
-    let target = path.resolve(arg);
-    let group = [], temp, files;
+async function convert(args: string[]) {
+  let proms = [];
+  const folderProms = [];
+  for (const arg of args) {
+    const target = path.resolve(arg);
+    const group = [];
+    let temp;
+    let files;
     try {
       [temp, files] = await get_temp_and_files(target);
     }
@@ -136,10 +147,9 @@ async function convert(args) {
       continue;
     }
     console.log(`[${new Date().toLocaleTimeString()}] Processing ${arg}...`);
-    for (let file of files) {
+    for (const file of files) {
       if (proms.length >= 4) {
         await Promise.race(proms);
-        // proms = proms.filter(p => !p.done);
       }
       console.log(`[${new Date().toLocaleTimeString()}] Process ${file}`);
       let prom = revise_pic(file);
@@ -153,28 +163,30 @@ async function convert(args) {
       proms.push(prom);
       group.push(prom);
     }
-    if (temp) folder_proms.push(Promise.all(group).then(async () => {
-      try {
-        await fs.unlink(target);
-        let prev_cwd = process.cwd();
-        process.chdir(temp);
-        await seven_zip('a', target, '-y', '-mx=0', '*');
-        process.chdir(prev_cwd);
-        await rmrf(temp)
-      }
-      catch (e) { console.log(e); }
-    }));
+    if (temp) {
+      folderProms.push(Promise.all(group).then(async () => {
+        try {
+          await fs.unlink(target);
+          const prevCmd = process.cwd();
+          process.chdir(temp);
+          await seven_zip("a", target, "-y", "-mx=0", "*");
+          process.chdir(prevCmd);
+          await rmrf(temp);
+        }
+        catch (e) { console.log(e); }
+      }));
+    }
   }
-  await Promise.all(proms.concat(folder_proms));
+  await Promise.all(proms.concat(folderProms));
 }
 
-if (require.main === module) {
-  let pushd = process.cwd();
+function main() {
+  const pushd = process.cwd();
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
-  process.on('uncaughtException', err => {
+  process.on("uncaughtException", err => {
     rl.question(`uncaught: ${err}`, () => rl.close());
     process.chdir(pushd);
   });
@@ -182,4 +194,8 @@ if (require.main === module) {
     console.log(`[${new Date().toLocaleTimeString()}] Complete.`);
     rl.close();
   });
+}
+
+if (require.main === module) {
+  main();
 }
